c07c29940cb6fcdce59c09a68cb9a369c49ed847
James Tillett <jtillett@endeavoreng.com>
2025-06-05 10:26:48 -0600

    Added abbkm connector code from myConnector example with only changing the build.gradle.kts file.

---
+++ eei-seeq-link-connector-abbkm/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    java
+    distribution
+}
+
+group = "com.abbkmv4.seeq.link.connector"
+version = "100.0.0-SNAPSHOT"
+
+dependencies {
+    compileOnly("com.seeq.link:seeq-link-sdk:${project.properties["seeqLinkSDKVersion"]}")
+    testImplementation("com.seeq.link:seeq-link-sdk:${project.properties["seeqLinkSDKVersion"]}")
+    testImplementation("org.mockito:mockito-core:4.1.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+    testImplementation("org.assertj:assertj-core:3.19.0")
+    testImplementation(testFixtures("com.seeq.link:seeq-link-sdk:${project.properties["seeqLinkSDKVersion"]}"))
+}
+
+tasks {
+    jar {
+        doFirst {
+            val files = configurations.runtimeClasspath.get().files
+            if (files.isNotEmpty()) {
+                manifest.attributes["Class-Path"] = files.joinToString(" ") { "lib/${it.name}" }
+            }
+        }
+    }
+
+    withType<Jar>().configureEach {
+        manifest.attributes(
+            "Version" to project.version,
+            "Minimum-Seeq-Link-SDK-Version" to "${project.properties["seeqLinkSDKVersion"]}"
+        )
+    }
+
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+}
+
+distributions {
+    main {
+        contents {
+            from(tasks.jar)
+            into("lib") {
+                from(configurations.runtimeClasspath)
+            }
+        }
+    }
+}
